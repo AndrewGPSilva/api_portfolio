0b0102fff4bd73fa699d7590be636a1a0659d5fb
Andrew Gomes <andrewgomes1328@gmail.com>
2023-11-09 21:14:23 -0300

    Metodo delete e procurar por id, simples até o momento

--- src/main/java/com/portfolio/api/Controllers/ProjetoController.java
+++ src/main/java/com/portfolio/api/Controllers/ProjetoController.java
@@ -9,19 +9,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/projetos")
+@RequestMapping
 public class ProjetoController {
 
     @Autowired
     private ProjetoRepository repository;
 
-    @GetMapping
+    @GetMapping("/api/projetos")
     public List<Projeto> getAll() {
         return repository.findAll();
     }
 
-    @PostMapping
+    @PostMapping("/api/projetos")
     public Projeto create(@RequestBody Projeto projeto) {
         return repository.save(projeto);
+    }
+
+    @GetMapping("/api/projetos/{id}")
+    public Projeto show(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/api/projetos/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
